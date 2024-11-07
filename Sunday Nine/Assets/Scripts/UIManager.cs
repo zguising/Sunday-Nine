cd4dd030f559ab83cc5e6565d756977f5b32a593
Zach Guisinger <zach8205@gmail.com>
2024-11-07 17:24:18 -0500

    Final Push before experimenting between scenes

--- Sunday Nine/Assets/Scripts/UIManager.cs
+++ Sunday Nine/Assets/Scripts/UIManager.cs
@@ -1,18 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
+
+    [SerializeField] private TextMeshProUGUI strokeCountText;
+    [SerializeField] private TextMeshProUGUI holeScoreText;
+
+    private int strokeCount = 0;
+    private int currentHole = 1;
+    private int totalHoles = 9;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        UpdateStrokeUI();
+    }
+
+    public void AddStroke()
+    {
+        strokeCount++;
+        Debug.Log("Stroke added " + strokeCount);
+        UpdateStrokeUI();
+    }
+
+    private void UpdateStrokeUI()
+    {
+        strokeCountText.text = "Strokes: " + strokeCount;
+        Debug.Log("Stroke upadated to " + strokeCountText.text);
+        holeScoreText.text = "Hole: " + currentHole;
     }
 
     // Update is called once per frame
-    void Update()
+    public void NexHtHole()
     {
-        
+        if (currentHole < totalHoles)
+        {
+            currentHole++;
+            SceneManager.LoadScene("Hole " + currentHole);
+            UpdateStrokeUI();
+        }
     }
 }
